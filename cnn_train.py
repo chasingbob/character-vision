80c4ddd32c9bb131c1f5202943a0d0d90e8134ba
Chasingbob <dries.cronje@outlook.com>
2016-12-07 01:34:32 +0200

    added random circles and added a buffer between front and back colours

--- cnn_train.py
+++ cnn_train.py
@@ -3,14 +3,16 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
+import numpy as np
+import matplotlib.pyplot as plt
 
 img_width, img_height = 64, 64
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
-nb_epoch = 300
+nb_train_samples = 4000
+nb_validation_samples = 1600
+nb_epoch = 50
 
 model = Sequential()
 model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height, 3)))
@@ -25,6 +27,10 @@ model.add(Convolution2D(64, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+model.add(Convolution2D(128, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
@@ -36,12 +42,12 @@ model.compile(loss='binary_crossentropy',
 				optimizer='rmsprop',
 				metrics=['accuracy'])
 
-#train_datagen = ImageDataGenerator(
-#		rescale=1./255,
-#		shear_range=0.2,
-#		zoom_range=0.2,
-#		horizontal_flip=False)
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(
+		rescale=1./255,
+	        shear_range=0.1,
+		zoom_range=0.1,
+		horizontal_flip=False)
+#train_datagen = ImageDataGenerator(rescale=1./255)
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -60,12 +66,29 @@ validation_generator = test_datagen.flow_from_directory(
 
 start = time.time()
 
-model.fit_generator(
-		train_generator,
-		samples_per_epoch=nb_train_samples,
-		nb_epoch=nb_epoch,
-		validation_data=validation_generator,
-		nb_val_samples=nb_validation_samples)
+not_found = True
+val_acc = []
+acc = []
+
+hist = model.fit_generator(
+	train_generator,
+	samples_per_epoch=nb_train_samples,
+	nb_epoch=nb_epoch,
+	validation_data=validation_generator,
+	nb_val_samples=nb_validation_samples)
+
+
+print(hist.history.keys())
+
+plt.plot(hist.history['acc'])
+#plt.plot(acc)
+#plt.plot(val_acc)
+plt.plot(hist.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 model.save_weights('weights.hdf5')
 
--- generate.py
+++ generate.py
@@ -9,6 +9,9 @@ import random
 
 random.seed(5)
 
+class Options:
+    light, dark, none = range(3)
+
 def get_random_font():
     allowed_fonts = [
         'dejavu',
@@ -21,7 +24,6 @@ def get_random_font():
         fonts = [f for f in os.listdir(join(base_dir, font_dir)) if f.endswith('.ttf')]
         if len(fonts) > 0:
             font_name = fonts[random.randint(0, len(fonts)-1)]
-            print(font_name)
             font_path = join(join(base_dir, font_dir), font_name)
             font = ImageFont.truetype(font_path, random.randint(12,55))
             return font
@@ -31,22 +33,55 @@ def get_random_font():
 
     return font
 
+def get_random_options():
+    if random.randint(0,1) == 0:
+        return Options.light, Options.dark
+    return Options.dark, Options.light
 
-def get_random_color():
-	return(random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
+def get_random_color(option=Options.none):
+    if option == Options.none:
+        return(random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
+    elif option == Options.light:
+        return (random.randint(150, 255), random.randint(150, 255), random.randint(150, 255))
+    return (random.randint(0,105), random.randint(0,105), random.randint(0,105))
 
 def get_random_pos():
-	return (random.randint(1, 150),random.randint(1, 150))
+    return (random.randint(1, 150),random.randint(1, 150))
+
+def get_random_line():
+    return (get_random_pos(), get_random_pos())
 
 def generate_random_img(filename, text):
-	font = get_random_font()
-	background = get_random_color()
-	colour = get_random_color()
-	img = Image.new("RGBA", (200,200), background)
-	draw = ImageDraw.Draw(img)
-	draw.text(get_random_pos(), text, colour , font=font)
-	draw = ImageDraw.Draw(img)
-	img.save(filename)
+    font = get_random_font()
+    back_option, front_option = get_random_options()
+    back_colour = get_random_color(option=back_option)
+    front_colour = get_random_color(option=front_option)
+    img = Image.new("RGBA", (200,200), back_colour)
+    draw = ImageDraw.Draw(img)
+    
+    nb_lines = random.randint(0, 5)
+    
+    for i in range(nb_lines):
+        x1 = random.randint(1,200)
+        x2 = random.randint(1,200)
+        y1 = random.randint(1,200)
+        y2 = random.randint(1,200)
+        draw.line((x1, y1, x2, y2), fill=get_random_color(option=back_option),width=random.randint(1,20))
+
+    nb_circles = random.randint(0, 5)
+
+    for i in range(nb_circles):
+        x1 = random.randint(1,150)
+        x2 = random.randint(x1, 200)
+        y1 = random.randint(1,150)
+        y2 = random.randint(y1, 200)
+        draw.ellipse((x1, y1, x2, y2), fill=get_random_color(option=back_option))
+
+
+    draw.text(get_random_pos(), text, front_colour , font=font)
+    draw = ImageDraw.Draw(img)
+    img.save(filename)
+
 
 def generate_batch(directory, basename, nb_images=1000):
 	path = os.path.join(directory, basename)
@@ -68,11 +103,27 @@ def make_sure_path_exists(path):
 
 def main():
 	
-	generate_batch('data/train','0',1000)
-	generate_batch('data/train','1',1000)
+	generate_batch('data/train','0',10000)
+	generate_batch('data/train','1',10000)
+	generate_batch('data/train','2',10000)
+	generate_batch('data/train','3',10000)
+	generate_batch('data/train','4',10000)
+	generate_batch('data/train','5',10000)
+	generate_batch('data/train','6',10000)
+	generate_batch('data/train','7',10000)
+	generate_batch('data/train','8',10000)
+	generate_batch('data/train','9',10000)
 
-	generate_batch('data/validation','0',400)
-	generate_batch('data/validation','1',400)
+	generate_batch('data/validation','0',2000)
+	generate_batch('data/validation','1',2000)
+	generate_batch('data/validation','2',2000)
+	generate_batch('data/validation','3',2000)
+	generate_batch('data/validation','4',2000)
+	generate_batch('data/validation','5',2000)
+	generate_batch('data/validation','6',2000)
+	generate_batch('data/validation','7',2000)
+	generate_batch('data/validation','8',2000)
+	generate_batch('data/validation','9',2000)
 
 
 if __name__ == '__main__':
